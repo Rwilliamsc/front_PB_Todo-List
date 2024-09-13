d2a0625ff4b8fec2bd5279d7afa56a5c779f81bf
Rwilliamsc <rwilliam.sc@gmail.com>
2024-09-13 00:52:42 -0300

    feat: adjust

--- src/context/AuthContextType.tsx
+++ src/context/AuthContextType.tsx
@@ -30,6 +30,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   return <AuthContext.Provider value={{ user, token, login, logout }}>{children}</AuthContext.Provider>;
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) throw new Error("useAuth must be used within an AuthProvider");
